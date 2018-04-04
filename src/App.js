d91ec365cce2670c55cbbc22bf3db4e93a80e59b
Jason Sio <son8292003@gmail.com>
2018-04-05 02:09:46 +1000

    added ReactTable to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+  }
+	
   render() {
     return (
       <div className="App">
+        <ReactTable
+          columns={[
+			{
+              Header: "Title",
+              accessor: "title.rendered"
+            },
+            {
+              Header: "Author ID",
+              accessor: "id"
+            },
+            {
+              Header: "Post Date",
+              accessor: "modified"
+            }
+          ]}
 
+          onFetchData={this.fetchData} 
+          defaultPageSize={10}
+          className="project-list -striped -highlight"
+        />
       </div>
     );
   }

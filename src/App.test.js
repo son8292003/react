a13a6f69e111bd07d9e8237ead89ff7a9f96b314
Jason Sio <son8292003@gmail.com>
2018-04-05 02:08:53 +1000

    add test case for project list table

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactTable from "react-table";
+import {shallow} from 'enzyme';
+import { expect } from 'chai';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders a table', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(ReactTable)).to.have.length(1);
 });
